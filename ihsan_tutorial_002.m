bd79c849468461ed22c5f4d1e82bafd30fb94dcc
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2017-02-06 09:03:34 +0900

    Tuts

---
+++ ihsan_tutorial_002.m
@@ -0,0 +1,31 @@
+clear variables;
+close all;
+clc; % clear console
+
+% 
+% x = 5;
+% v = [1 2 3];
+% v2 = [1;2;3];
+% s = size(v);
+
+% fprintf('%g \n', s);
+
+% v(1,1) 
+
+% v = [1;2;3]
+% v=v'
+
+
+% Matrix test
+A = [1 2; 3 4];
+B = [5 6; 7 8];
+C = [9 10; 11 12];
+
+A - B;
+A*B;
+
+B2 = [1 2; 4 5; 6 7;];
+size(B2)
+size(A)
+
+A+B
